ead5986f2e98d9d9adaad9bc81c59898e0524d08
AlvinGary <alvingary201@gmail.com>
2023-12-27 16:01:40 +0700

    init

---
+++ lib/views/pages/firstscreen.dart
@@ -0,0 +1,15 @@
+part of 'pages.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/views/pages/pages.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+part 'firstscreen.dart';
+part 'secondscreen.dart';
+part 'thirdscreen.dart';
